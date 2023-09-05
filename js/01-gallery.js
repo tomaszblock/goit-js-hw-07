c89ad65509e8b1fbbec5744258cf462aeb73a106
tomaszblock <127046228+tomaszblock@users.noreply.github.com>
2023-09-05 13:14:14 +0200

    fixed to one loop

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,33 +3,56 @@ import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
 
-const galerry = document.querySelector(".gallery");
+const gallery = document.querySelector(".gallery");
 
 const items = [];
 
+//option 1
+
 for (let i = 0; i < galleryItems.length; i++) {
+  console.log(i)
   const newItem = document.createElement("div");
   newItem.classList.add("gallery__item");
-  items.push(newItem);
-}
-galerry.append(...items);
-
-const itemList = document.querySelectorAll(".gallery__item");
-
-for (let i = 0; i < itemList.length; i++) {
+  gallery.append(newItem);
+  const itemList = gallery.lastElementChild;
   const newLink = document.createElement("a");
   newLink.classList.add("gallery__link");
   newLink.setAttribute("href", galleryItems[i].original);
-  itemList[i].append(newLink);
-}
-
-const linksList = document.querySelectorAll(".gallery__link");
-
-for (let i = 0; i < linksList.length; i++) {
+  itemList.append(newLink);
+  const linksList = itemList.lastElementChild;
   const newImg = document.createElement("img");
   newImg.setAttribute("src", galleryItems[i].preview);
   newImg.setAttribute("data-source", galleryItems[i].original);
   newImg.setAttribute("alt", galleryItems[i].description);
   newImg.classList.add("gallery__image");
-  linksList[i].append(newImg);
+  linksList.append(newImg);
 }
+
+//option 2
+
+// for (let i = 0; i < galleryItems.length; i++) {
+//   const newItem = document.createElement("div");
+//   newItem.classList.add("gallery__item");
+//   items.push(newItem);
+// }
+// galerry.append(...items);
+
+// const itemList = document.querySelectorAll(".gallery__item");
+
+// for (let i = 0; i < itemList.length; i++) {
+//   const newLink = document.createElement("a");
+//   newLink.classList.add("gallery__link");
+//   newLink.setAttribute("href", galleryItems[i].original);
+//   itemList[i].append(newLink);
+// }
+
+// const linksList = document.querySelectorAll(".gallery__link");
+
+// for (let i = 0; i < linksList.length; i++) {
+//   const newImg = document.createElement("img");
+//   newImg.setAttribute("src", galleryItems[i].preview);
+//   newImg.setAttribute("data-source", galleryItems[i].original);
+//   newImg.setAttribute("alt", galleryItems[i].description);
+//   newImg.classList.add("gallery__image");
+//   linksList[i].append(newImg);
+// }
