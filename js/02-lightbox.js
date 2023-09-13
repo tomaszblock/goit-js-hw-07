897c02b866d7eb9a7c0a28e07bcb57d35a0cb8d1
tomaszblock <blocktomasz@gmail.com>
2023-09-13 20:39:35 +0200

    added hw2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,5 @@
-import { galleryItems } from './gallery-items.js';
-import SimpleLightbox from "simplelightbox";
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
 console.log(galleryItems);
@@ -7,20 +7,22 @@ console.log(galleryItems);
 const parent = document.querySelector(".gallery");
 
 for (let item of galleryItems) {
-  
-    let newLink = document.createElement("a");
-    newLink.classList.add("gallery__item");
-    newLink.setAttribute("href", item.original);
-  
-    let newImage = document.createElement("img");
-    newImage.classList.add("gallery__image");
-    newImage.setAttribute("src", item.preview);
-    newImage.setAttribute("alt", item.description);
-  
-    parent.append(newLink);
-    newLink.append(newImage);
-  }
+  let newItem = document.createElement("li");
+  let newLink = document.createElement("a");
+  newLink.classList.add("gallery__item");
+  newLink.setAttribute("href", item.original);
 
-  (function() {
-    var $gallery = new SimpleLightbox('.gallery a', {});
-})();
+  let newImage = document.createElement("img");
+  newImage.classList.add("gallery__image");
+  newImage.setAttribute("src", item.preview);
+  newImage.setAttribute("alt", item.description);
+
+  newLink.append(newImage);
+  newItem.append(newLink);
+  parent.append(newItem);
+}
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
